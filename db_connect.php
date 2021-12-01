5c2d99cdf39ef841a3f3b40108ac0ba6e82ffeaa
Cosaki24 <64184560+Cosaki24@users.noreply.github.com>
2021-12-01 22:34:25 +0300

    added a password for my local MySQL server and removed it.

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
  
- $db = new mysqli("localhost","root","Uzumymw312978");
+ $db = new mysqli("localhost","root","");
    if($db->connect_errno > 0){
          die('Unable to connect to database [' . $db->connect_error . ']');  } 
      
